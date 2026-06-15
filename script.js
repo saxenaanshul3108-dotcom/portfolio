a08689230dbe4136e5f1ed0297fbfd7e245e2f06
saxenaanshul3108-dotcom <saxenaanshul3108@gmail.com>
2026-06-15 13:01:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,12 @@ function type() {
 }
 
 type();
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
